0c0ad95ac3335d9a2634370063ee983728a096da
sungho Moon <moon@7959.ml>
2020-11-22 20:41:02 +0900

    ef

--- index.js
+++
@@ -1,7 +0,0 @@
-var redbutton = document.getElementById("redbutton");
-for( var x=0; x < redbutton.length; x++ )
- redbutton[x].onclick = function(){
-    lack = setInterval(function() {
-        window.open('explode.html', '_blank')
-     }, 3000);
- };
